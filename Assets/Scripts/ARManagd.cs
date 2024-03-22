ee08aa708a29af2cee211ffa5a24b63194d33a62
anTe <77624934+Ante-237@users.noreply.github.com>
2024-03-23 00:23:04 +0200

    UI completion

--- Assets/Scripts/ARManagd.cs
+++ Assets/Scripts/ARManagd.cs
@@ -3,16 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 using UnityEngine.XR.MagicLeap;
 
 public class ARManagd : MonoBehaviour
 {
+    [Header("AR Manager Settings")] [SerializeField]
+    private string[] TrackedNames = { "angola", "social_commons" };
+    [SerializeField] private ARTrackedImageManager TrackedImageManager;
+
+
+    [Header("Tracked Image Prefabs")] [SerializeField]
+    private List<GameObject> DetectionPrefabs = new List<GameObject>();
+
+    private GameObject CurrentTrackedPrefab;
+    
+    [Header("Reference World Images")]
     public float RotationSpeed = 15f;
     public Button Rightbutton;
     public Button LeftButton;
     public Button InfoButton;
     public Button AudioButton;
 
+    [Header("World Objects")]
     private GameObject WorldObject;
     private GameObject CanvasInfo;
     private GameObject CameraObj;
@@ -20,24 +34,65 @@ public class ARManagd : MonoBehaviour
 
     private void Start()
     {
+        
+        // set current track prefab here
+        CurrentTrackedPrefab = DetectionPrefabs[1];
+
+        TrackedImageManager = GetComponent<ARTrackedImageManager>();
+        
         Rightbutton.onClick.AddListener(RotateRight);
         LeftButton.onClick.AddListener(RotateLeft);
         InfoButton.onClick.AddListener(ShowInfo);
         AudioButton.onClick.AddListener(PlayAudio);
         
         CameraObj = GameObject.FindGameObjectWithTag("MainCamera");
+
+
+        TrackedImageManager.trackedImagesChanged += (ARTrackedImagesChangedEventArgs args) =>
+        {
+            TrackedImageManager.trackedImagePrefab = CurrentTrackedPrefab;
+        };
+
+
+    }
+
+    private void UpdateCurrentPrefab()
+    {
+        
+        foreach (ARTrackedImage tracked in TrackedImageManager.trackables)
+        {
+            Debug.Log("Tracked Image is :" + tracked.name);
+            
+            if (tracked.name == TrackedNames[0])
+            {
+               // CurrentTrackedPrefab = DetectionPrefabs[0];
+                TrackedImageManager.trackedImagePrefab = CurrentTrackedPrefab;
+            }
+
+            if (tracked.name == TrackedNames[1])
+            {
+                CurrentTrackedPrefab = DetectionPrefabs[1];
+                TrackedImageManager.trackedImagePrefab = DetectionPrefabs[1];
+            }
+        }
     }
 
     void Update()
     {
+        UpdateCurrentPrefab();
+        
         if (Time.frameCount % 20 == 0)
         {
             if (WorldObject == null)
             {
                 WorldObject = GameObject.FindGameObjectWithTag("ModelObject");
-                Audio = WorldObject.transform.GetChild(1).GetComponent<AudioSource>();
-                CanvasInfo = GameObject.FindGameObjectWithTag("ModelUI");
-                CanvasInfo.gameObject.SetActive(false);
+                if (WorldObject.name == "FuseboxMesh")
+                {
+                    Audio = WorldObject.transform.GetChild(1).GetComponent<AudioSource>();
+                    CanvasInfo = GameObject.FindGameObjectWithTag("ModelUI");
+                    CanvasInfo.gameObject.SetActive(false);
+                }
+             
             }
         }
 
